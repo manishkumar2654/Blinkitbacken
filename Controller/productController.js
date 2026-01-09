d2cca958b889cbdbc07a5042d253f7ce419240f7
manishkumar2654 <Hairbymanish@gmail.com>
2026-01-09 08:30:07 +0530

    fixed candy category api

--- Controller/productController.js
+++ Controller/productController.js
@@ -1,20 +1,32 @@
 const productModel = require("../Model/productModel");
 
+// 🔹 Home page products
 const displayAlldata = async (req, res) => {
-    const productData = await productModel.find();
-    res.send(productData)
-    
-}
+  const productData = await productModel.find();
+  res.send(productData);
+};
 
+// 🔹 Single product
+const ProudctDisplay = async (req, res) => {
+  const { id } = req.query;
+  const productDisplay = await productModel.findById(id);
+  res.send(productDisplay);
+};
 
-const ProudctDisplay =async (req, res) => {
-    const { id } = req.query;
-    const productDisplay = await productModel.findById(id)
-    res.send(productDisplay)
-    
-}
+// 🔹 Candies & Gums category
+const candyDisplay = async (req, res) => {
+  try {
+    const products = await productModel.find({
+      category: "candies-gums",
+    });
+    res.status(200).json(products);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
 
-module.exports={
-    displayAlldata,
-    ProudctDisplay
-}
+module.exports = {
+  displayAlldata,
+  ProudctDisplay,
+  candyDisplay,
+};
--- Routes/blinkitProductRoutes.js
+++ Routes/blinkitProductRoutes.js
@@ -1,12 +1,9 @@
 const express = require("express");
 const route = express.Router();
-const productController = require("../Controller/productController")
+const productController = require("../Controller/productController");
 
-route.get("/homedisplaydata",productController.displayAlldata );
-route.get("/productdisplay",productController.ProudctDisplay );
+route.get("/homedisplaydata", productController.displayAlldata);
+route.get("/productdisplay", productController.ProudctDisplay);
+route.get("/candy", productController.candyDisplay);
 
-
-
-
-
-module.exports =route;
+module.exports = route;
