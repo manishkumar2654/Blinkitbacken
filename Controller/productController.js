1e880a7e0c5e3ee3ee64dcfd79aec99b1c291a19
manishkumar2654 <Hairbymanish@gmail.com>
2026-01-10 08:43:37 +0530

    Added PaanShop API and updated controllers

--- Controller/productController.js
+++ Controller/productController.js
@@ -25,8 +25,96 @@ const candyDisplay = async (req, res) => {
   }
 };
 
+
+const snacksDisplay = async (req, res) => {
+  try {
+    const data = await productModel.find({ category: "snacks-munchies" });
+    res.send(data);
+  } catch (err) {
+    res.status(500).send({ error: "Server Error" });
+  }
+};
+
+
+
+
+
+const dairyDisplay = async (req, res) => {
+  try {
+    const data = await productModel.find({ category: "dairy-bread-eggs" });
+    res.send(data);
+  } catch (err) {
+    res.status(500).send({ error: "Server Error" });
+  }
+};
+
+const coldDrinksDisplay = async (req, res) => {
+  try {
+    const data = await productModel.find({ category: "cold-drinks-juices" });
+    res.send(data);
+  } catch (err) {
+    res.status(500).send({ error: "Server Error" });
+  }
+};
+
+
+const chickenMeatFishDisplay = async (req,res)=>{
+  try { const data = await productModel.find({category:"chicken-meat-fish"}); res.send(data); }
+  catch(err){ res.status(500).send({error:"Server Error"}); }
+};
+
+const petCareDisplay = async (req,res)=>{
+  try { const data = await productModel.find({category:"pet-care"}); res.send(data); }
+  catch(err){ res.status(500).send({error:"Server Error"}); }
+};
+
+// Repeat same for other categories, e.g.
+const babyCareDisplay = async (req,res)=>{ const data = await productModel.find({category:"baby-care"}); res.send(data); };
+const magazinesDisplay = async (req,res)=>{ const data = await productModel.find({category:"magazines"}); res.send(data); };
+const sweetToothDisplay = async (req,res)=>{ const data = await productModel.find({category:"sweet-tooth"}); res.send(data); };
+const beautyCosmeticsDisplay = async (req,res)=>{ const data = await productModel.find({category:"beauty-cosmetics"}); res.send(data); };
+const toysGamesDisplay = async (req,res)=>{ const data = await productModel.find({category:"toys-games"}); res.send(data); };
+const vegetablesFruitsDisplay = async (req,res)=>{ const data = await productModel.find({category:"vegetables-fruits"}); res.send(data); };
+const bakeryBiscuitsDisplay = async (req,res)=>{ const data = await productModel.find({category:"bakery-biscuits"}); res.send(data); };
+const frozenDessertsDisplay = async (req,res)=>{ const data = await productModel.find({category:"frozen-desserts-icecream"}); res.send(data); };
+const pharmaWellnessDisplay = async (req,res)=>{ const data = await productModel.find({category:"pharma-wellness"}); res.send(data); };
+const personalCareDisplay = async (req,res)=>{ const data = await productModel.find({category:"personal-care"}); res.send(data); };
+const digitalGoodsDisplay = async (req,res)=>{ const data = await productModel.find({category:"digital-goods"}); res.send(data); };
+const cleaningEssentialsDisplay = async (req,res)=>{ const data = await productModel.find({category:"cleaning-essentials"}); res.send(data); };
+const attaRiceDalDisplay = async (req,res)=>{ const data = await productModel.find({category:"atta-rice-dal"}); res.send(data); };
+const paanshopDisplay = async (req,res)=>{ const data = await productModel.find({category:"paan-corner"}); res.send(data); };
+
+
+
+
+
+
 module.exports = {
   displayAlldata,
   ProudctDisplay,
   candyDisplay,
+  snacksDisplay,
+  dairyDisplay,
+  coldDrinksDisplay,
+  chickenMeatFishDisplay,
+  petCareDisplay,
+  babyCareDisplay,
+  magazinesDisplay,
+  sweetToothDisplay,
+  beautyCosmeticsDisplay,
+  toysGamesDisplay,
+  vegetablesFruitsDisplay,
+  bakeryBiscuitsDisplay,
+  frozenDessertsDisplay,
+  pharmaWellnessDisplay,
+  personalCareDisplay,
+  digitalGoodsDisplay,
+  cleaningEssentialsDisplay,
+  attaRiceDalDisplay,
+  paanshopDisplay,
+  
+  
+  
+  
+  
 };
--- Routes/blinkitProductRoutes.js
+++ Routes/blinkitProductRoutes.js
@@ -4,6 +4,39 @@ const productController = require("../Controller/productController");
 
 route.get("/homedisplaydata", productController.displayAlldata);
 route.get("/productdisplay", productController.ProudctDisplay);
+//candy
 route.get("/candy", productController.candyDisplay);
+
+// Snacks & Munchies
+route.get("/snacks", productController.snacksDisplay);
+
+// Dairy, Bread & Eggs
+route.get("/dairy", productController.dairyDisplay);
+
+
+
+// Cold Drinks & Juices
+route.get("/cold-drinks", productController.coldDrinksDisplay);
+route.get("/chicken-meat-fish", productController.chickenMeatFishDisplay);
+route.get("/pet-care", productController.petCareDisplay);
+route.get("/baby-care", productController.babyCareDisplay);
+route.get("/magazines", productController.magazinesDisplay);
+route.get("/sweet-tooth", productController.sweetToothDisplay);
+route.get("/beauty-cosmetics", productController.beautyCosmeticsDisplay);
+route.get("/toys-games", productController.toysGamesDisplay);
+route.get("/vegetables-fruits", productController.vegetablesFruitsDisplay);
+route.get("/bakery-biscuits", productController.bakeryBiscuitsDisplay);
+route.get("/frozen-desserts-icecream", productController.frozenDessertsDisplay);
+route.get("/pharma-wellness", productController.pharmaWellnessDisplay);
+route.get("/personal-care", productController.personalCareDisplay);
+route.get("/digital-goods", productController.digitalGoodsDisplay);
+route.get("/cleaning-essentials", productController.cleaningEssentialsDisplay);
+route.get("/atta-rice-dal", productController.attaRiceDalDisplay);
+route.get("/pann", productController.paanshopDisplay);
+
+
+
+
+
 
 module.exports = route;
